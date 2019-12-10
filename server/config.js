744e975343fcd79bc1a251ab8e2f256d8da2f6a3
ericgg <eric.grynberg@gmail.com>
2019-12-09 21:07:53 -0300

    change bd3

--- server/config.js
+++ server/config.js
@@ -2,6 +2,6 @@
 module.exports = {
   AUTH0_DOMAIN: 'inspx2.auth0.com', // e.g., kmaida.auth0.com
   AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://insp:147258a@ds257808.mlab.com:57808/inspdemo',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://insp:147258a@ds253388.mlab.com:57808/inspdemo',
   NAMESPACE: 'http://myapp.com/roles'
 };
